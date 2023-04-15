c329fa8701f86fdd6ffa8257b895797b530c2a6d
Vladimir <vlk@mail.ru>
2023-04-15 15:52:13 +0300

    Update iptabless.sh

--- etc/RU/iptabless.sh
+++ etc/RU/iptabless.sh
@@ -9,7 +9,7 @@ do
 done
 iptables -L -n -v --line-numbers
 iptables -I INPUT 1 -i eth1 -p udp -m multiport --dports 5040:5090 -m set ! --match-set russia_run src -j LOG --log-prefix "BAD_SIP_No_Russia-IPSET: " --log-level 6
-#iptables -I INPUT 2 -i eth1 -p udp -m multiport --dports 5040:5090 -m set ! --match-set russia_run src -j DROP
+iptables -I INPUT 2 -i eth1 -p udp -m multiport --dports 5040:5090 -m set ! --match-set russia_run src -j DROP
 iptables -L -n -v --line-numbers
 
 
